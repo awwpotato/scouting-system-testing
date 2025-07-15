a3698ed6f38ff3a4986fe66ee9c54e57603f294c
awwpotato <awwpotato@voidq.com>
2025-07-14 21:23:46 -0700

    check ci

--- nix/default.nix
+++ nix/default.nix
@@ -3,8 +3,12 @@
   imports = [ ./formatter.nix ];
 
   perSystem =
-    { pkgs, ... }:
+    { pkgs, config, ... }:
     {
+      # Build all packages with 'nix flake check' instead of only verifying they
+      # are derivations.
+      checks = config.packages;
+
       packages.default = pkgs.callPackage ./package.nix { inherit self; };
     };
 }
